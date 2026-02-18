c8d9635e8e6bd4ba77918339f6defeaadf44e7c5
greene11-poe <ndang7863@gmail.com>
2026-02-18 13:59:52 -0800

    Implementation: BN Node Populating

--- diagnostics.py
+++ diagnostics.py
@@ -6,12 +6,22 @@ class Diagnostics:
     """ Use a Bayesian network to diagnose between three lung diseases """
 
     def __init__(self):
-        self.nodes = ["Asia", "Smoking", "TB", "Lung cancer", "TB or Cancer", "X-ray result", "Dyspnea"]
+        self.network = BayesNet([
+            # (names, parents, prob)
+            ('Asia', '', 0.01),
+            ('Smoking', '', 0.5),
+            ('TB', 'Asia', {T: 0.05, F: 0.01}),
+            ('LungCancer', 'Smoking', {T: 0.6, F: 0.3}),
+            ('Bronchitis', 'Smoking', {T: 0.6, F: 0.3}),
+            ('TBorCancer', ['TB', 'LungCancer'], 
+            {(T, T): 1.0, (T, F): 1.0, (F, T): 1.0, (F, F): 0.0}),
+            ('XRay Result', 'TBorCancer', {T: 0.99, F: 0.05}),
+            ('Dyspnea', ['TBorCancer', 'Bronchitis'], 
+            {(T, T): 0.9, (T, F): 0.7, (F, T): 0.8, (F, F): 0.1})
+        ])
 
-
-    def diagnose (self, asia, smoking, xray, dyspnea):
+    def diagnose (self, visit_to_asia, smoking, xray_result, dyspnea):
         # To be implemented by the student
-
         # return either CASE SENSITIVE TB, Cancer, Bronchitis
         # return prob 0-1
         diseaseResult = "test"
