d51f6a5eee868dd9c4ed6c15d75b0b461485d00c
greene11-poe <ndang7863@gmail.com>
2026-02-18 14:49:59 -0800

    Enumeration ask implementation
    Note: provided probabilities for cancer are incorrect (0.6, 0.3)
    p(+c|s) should be (0.1, 0.01)
    
    Autograder: 25/25

--- diagnostics.py
+++ diagnostics.py
@@ -11,19 +11,43 @@ class Diagnostics:
             ('Asia', '', 0.01),
             ('Smoking', '', 0.5),
             ('TB', 'Asia', {T: 0.05, F: 0.01}),
-            ('LungCancer', 'Smoking', {T: 0.6, F: 0.3}),
+            ('Cancer', 'Smoking', {T: 0.1, F: 0.01}),
             ('Bronchitis', 'Smoking', {T: 0.6, F: 0.3}),
-            ('TBorCancer', ['TB', 'LungCancer'], 
+            ('TBorCancer', ['TB', 'Cancer'], 
             {(T, T): 1.0, (T, F): 1.0, (F, T): 1.0, (F, F): 0.0}),
-            ('XRay Result', 'TBorCancer', {T: 0.99, F: 0.05}),
+            ('XRayResult', 'TBorCancer', {T: 0.99, F: 0.05}),
             ('Dyspnea', ['TBorCancer', 'Bronchitis'], 
             {(T, T): 0.9, (T, F): 0.7, (F, T): 0.8, (F, F): 0.1})
         ])
 
     def diagnose (self, visit_to_asia, smoking, xray_result, dyspnea):
-        # To be implemented by the student
-        # return either CASE SENSITIVE TB, Cancer, Bronchitis
-        # return prob 0-1
-        diseaseResult = "test"
-        diseaseProbability = 0.5
-        return [f"{diseaseResult}", diseaseProbability] # placeholder return value, to be replaced by the student
+        mapping = {
+            "Yes": T, "No": F,
+            "Abnormal": T, "Normal": F,
+            "Present": T, "Absent": F
+        }
+        
+        inputs = {
+            'Asia': visit_to_asia,
+            'Smoking': smoking,
+            'XRayResult': xray_result,
+            'Dyspnea': dyspnea
+        }
+
+        evidence = {}
+        for key, value in inputs.items():
+            if value != "NA":
+                evidence[key] = mapping[value]
+
+        probTB = enumeration_ask('TB', evidence, self.network)[T]
+        probCancer = enumeration_ask('Cancer', evidence, self.network)[T]
+        probBronchitis = enumeration_ask('Bronchitis', evidence, self.network)[T]
+
+        results = [
+            ("Bronchitis", probBronchitis),
+            ("Cancer", probCancer),
+            ("TB", probTB)
+        ]
+
+        likelyDisease = max(results, key=lambda x: x[1])
+        return [likelyDisease[0], likelyDisease[1]]
